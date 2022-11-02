906df982e7d5a110934521b9c588322e4ca8fc4f
wooseokchoe <chldntjr0425@gmail.com>
2022-11-02 20:42:22 +0900

    1-week : firstLast6

---
+++ 1주차/firstLast6.java
@@ -0,0 +1,12 @@
+public boolean firstLast6(int[] nums) {
+        if(is6InLastOrFist(nums)){
+            return true;
+        }
+        return false;
+}
+
+public boolean is6InLastOrFist(int[] nums){
+        int firstValue = nums[0];
+        int lastValue = nums[nums.length-1];
+        return firstValue == 6 || lastValue == 6;
+}
