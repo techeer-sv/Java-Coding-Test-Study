6844875b060e9c2d54e64febff43d087948630ab
hyunwoo <ashyonu@gmail.com>
2022-11-09 23:47:46 +0900

    1-week Array-1

---
+++ hyunwoo/1주차/commonEnd.java
@@ -0,0 +1,5 @@
+public boolean commonEnd(int[] a, int[] b) {
+  // if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1] )
+  //   return true;
+  return (a[0] == b[0] || a[a.length - 1] == b[b.length - 1] );
+}
---
+++ hyunwoo/1주차/sum2.java
@@ -0,0 +1,7 @@
+public int sum2(int[] nums) {
+   if(nums.length == 0)
+    return 0;
+  if(nums.length == 1)
+    return nums[0] + 0;
+  return nums[0] + nums[1];
+}
