1dcbc8b05d5e228eb9ed39cbb1271059745154df
Ellie <doris0707@naver.com>
2022-11-12 10:30:46 +0900

    sol: seriesUp

--- yulim/02/seriesUp.java
+++ yulim/02/seriesUp.java
@@ -1,11 +1,13 @@
 public int[] seriesUp(int n) {
     int[] arr = new int[n*(n+1)/2];
-    int index = 0;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 0; j < i; j++) {
-            arr[index + j] = j + 1;
+    int size = n*(n+1)/2;
+    for(int i=size-1, j=n, sub=1; i>=0; i--,j--){
+        if(j==0) {
+            j=n-sub;
+            sub++;
         }
-        index += i;
+        arr[i] = j;
     }
     return arr;
 }
+
