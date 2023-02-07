7f71b607ac94e0a737bbb64b02df6e9d21f19fb9
zyoon <develop_z@naver.com>
2023-02-07 17:59:40 +0900

    substring 문자열 자르기

---
+++ zyoon/10주차/PRO147355.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String t, String p) {
+
+        int size = t.length() - p.length() +1;
+        String[] array = new String[size];
+        int answer = 0;
+
+        for(int i = 0; i < size; i++){
+            
+        }
+
+        return answer;
+    }
+}
